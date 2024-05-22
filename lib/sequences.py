5c0e72f74b01027473277e9b4d6cd9aef3748abf
kudzaishe muzata <kudzaishemmuzata@gmail.com>
2024-05-22 13:17:04 +0300

    completed lab

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,8 @@
 #!/usr/bin/env python3
 
 def print_fibonacci(length):
-    pass
+    fibonacci = [0, 1]
+    while len(fibonacci) < length:
+        nxtnum = fibonacci[-1] + fibonacci[-2]
+        fibonacci.append(nxtnum)
+    print(fibonacci[:length])
